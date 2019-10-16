fe25fc5791a89fe1770968b768bd55c0c8d38a38
sandeep <sandeep@ashmar.in>
2019-10-16 10:42:29 +0530

    add

--- app/src/main/java/com/example/viewpagre2/Adapter.java
+++ app/src/main/java/com/example/viewpagre2/Adapter.java
@@ -5,23 +5,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
 public class Adapter extends PagerAdapter {
     Context context;
-    int[] s1 = {R.drawable.xx,R.drawable.yy,R.drawable.zz};
-
+    int[] s1 = {R.drawable.xx, R.drawable.yy, R.drawable.zz};
     public Adapter(Context context) {
         this.context = context;
     }
-
     @Override
     public int getCount() {
         return s1.length;
     }
-
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
         return view == object;
@@ -29,9 +25,9 @@ public class Adapter extends PagerAdapter {
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
-        ImageView imageView = new ImageView(context);
-       imageView.setImageResource(s1[position]);
-        container.addView(imageView);
+        ImageView imageView = new ImageView(context);//container is reference variable of class ViewGroup.
+        imageView.setImageResource(s1[position]);
+        container.addView(imageView);//here we are dynamically generating imageview & adding it with ViewGroup.
         return imageView;
     }
     @Override
--- app/src/main/java/com/example/viewpagre2/MainActivity.java
+++ app/src/main/java/com/example/viewpagre2/MainActivity.java
@@ -7,13 +7,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewPager = findViewById(R.id.viewpager1);
-        // String s1[]={"Monet","Maruti","Ashmar","Concentrix"};
         Adapter adapter = new Adapter(this);
         viewPager.setAdapter(adapter);
     }
